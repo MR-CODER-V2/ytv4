54c633965d81493620d7acf24c152e6be6f5ab36
MR-CODER-V2 <83626456+MR-CODER-V2@users.noreply.github.com>
2023-05-07 16:59:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5888188144:AAGy-gatdcobSPiUa9pT3aoItJoOpqs1898")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", "9544521"))
+    API_HASH = os.environ.get("API_HASH", "5cf32e97dc94510e46524f2286c95116")
     # Get these values from my.telegram.org
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
@@ -20,13 +20,13 @@ class Config(object):
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = ""
     # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
+    MAX_MESSAGE_LENGTH = 16348
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    SESSION_NAME = "UPLOADER-X-BOT"
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1358657527"))
+    SESSION_NAME = "test_v12bot"
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://test:test@cluster0.rc1urp8.mongodb.net/?retryWrites=true&w=majority")
     MAX_RESULTS = "50"
-    PREMIUM_USER = os.environ.get("PREMIUM_USER")
+    PREMIUM_USER = os.environ.get("PREMIUM_USER", "True")
